83bbd3b79d186ca5980ce9e0e3e3620bee66ac77
Lucas Kirby <lucas.kirby07@yahoo.com>
2026-02-18 14:18:04 -0500

    Added point click movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
-using UnityEngine.Windows;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 5;
+    [SerializeField] private Camera gameCam;
+
     private Animator anim;
     private Rigidbody2D rb;
+    private bool isMoving;
+    private Vector2 targetPos;
 
     private void Awake()
     {
@@ -17,5 +22,28 @@ public class Player : MonoBehaviour
         anim.SetFloat("Velocity", Mathf.Abs(rb.linearVelocity.x));
 
         transform.localScale = new Vector3(Mathf.Sign(rb.linearVelocity.x), transform.localScale.y, 1);
+    }
+
+    private void FixedUpdate()
+    {
+        if (isMoving)
+        {
+            Vector2 direction = (targetPos - (Vector2)transform.position).normalized;
+            rb.linearVelocity = direction * moveSpeed;
+            if (Vector2.Distance(transform.position, targetPos) < 0.1f)
+            {
+                rb.linearVelocity = Vector2.zero;
+                isMoving = false;
+            }
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            targetPos = gameCam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            isMoving = true;
+        }
     }
 }
